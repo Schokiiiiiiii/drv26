221586b60d724a502f248a589b40d6c5f4049d5a
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-03-30 19:51:38 +0200

    Started lab4 parrot exercise

--- material/lab_04/parrot_module/parrot.c
+++ material/lab_04/parrot_module/parrot.c
@@ -3,6 +3,7 @@
  * Parrot file
  */
 
+#include <asm-generic/errno-base.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -21,6 +22,15 @@
 static struct cdev cdev;
 static struct class *cl;
 
+// parrot data
+#define BASE_LENGTH	   8
+#define MAX_LENGTH	1024
+static struct {
+	struct device *clsdev;
+	ssize_t length;
+	uint8_t *data;
+} parrot_data;
+
 /**
  * @brief Read back previously written data in the internal buffer.
  *
@@ -34,9 +44,24 @@ static struct class *cl;
  *         or a negative error code
  */
 static ssize_t parrot_read(struct file *filp, char __user *buf, size_t count,
-			   loff_t *ppos)
-{
-	return 0;
+			   loff_t *ppos) {
+
+	// check arguments validity
+	if (count <= 0 || *ppos < 0 || *ppos >= parrot_data.length || buf == NULL)
+		return -EINVAL;
+
+	// check we don't read over length
+	if (*ppos + count > parrot_data.length)
+		return -EINVAL;
+
+	// copy to user
+	if (copy_to_user(buf, parrot_data.data + *ppos, count))
+		return -EFAULT;
+
+	// update ppos
+	*ppos += count;
+
+	return count;
 }
 
 /**
@@ -52,9 +77,31 @@ static ssize_t parrot_read(struct file *filp, char __user *buf, size_t count,
  *         or a negative error code
  */
 static ssize_t parrot_write(struct file *filp, const char __user *buf,
-			    size_t count, loff_t *ppos)
-{
-	return 0;
+			    size_t count, loff_t *ppos) {
+
+	// check arguments validity
+	if (count <= 0 || *ppos < 0 || *ppos >= parrot_data.length || buf == NULL)
+		return -EINVAL;
+
+	// check we don't go over MAX_LENGTH
+	if (*ppos + count > MAX_LENGTH)
+		return -EINVAL;
+
+	// realloc if necessary
+	if (*ppos + count > parrot_data.length) {
+		parrot_data.data = devm_krealloc(parrot_data.clsdev, (void *)parrot_data.data, *ppos + count, GFP_KERNEL);
+		if (parrot_data.data == NULL)
+			return -ENOMEM;
+	}
+
+	// copy from user space
+	if (copy_from_user(parrot_data.data + *ppos, buf, count))
+		return -EFAULT;
+
+	// update ppos
+	*ppos += count;
+
+	return count;
 }
 
 /**
@@ -77,8 +124,8 @@ static const struct file_operations parrot_fops = {
 	.llseek = default_llseek, // Use default to enable seeking to 0
 };
 
-static int __init parrot_init(void)
-{
+static int __init parrot_init(void) {
+
 	struct device *clsdev;
 	int err;
 
@@ -104,12 +151,21 @@ static int __init parrot_init(void)
 		goto err_device_create;
 	}
 
+	// store device
+	parrot_data.clsdev = clsdev;
+
 	cdev_init(&cdev, &parrot_fops);
 	err = cdev_add(&cdev, MAJMIN, 1);
 	if (err < 0) {
 		pr_err("Parrot: Adding char device failed (%d)\n", err);
 		goto err_cdev_add;
 	}
+
+	// create buffer with BASE_LENGTH bytes
+	parrot_data.data = devm_kmalloc(clsdev, BASE_LENGTH * sizeof(uint8_t), GFP_KERNEL);
+	if (parrot_data.data == NULL)
+		return -EBUSY;
+	parrot_data.length = BASE_LENGTH;
 
 	pr_info("Parrot ready!\n");
 
@@ -126,8 +182,8 @@ err_class_create:
 
 }
 
-static void __exit parrot_exit(void)
-{
+static void __exit parrot_exit(void) {
+
 	// Unregister the device
 	cdev_del(&cdev);
 	device_destroy(cl, MAJMIN);
