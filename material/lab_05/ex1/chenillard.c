e501c3cf9e2aa928ff7cbf16fc03561a3945b8b2
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-05-02 16:42:40 +0200

    Fixed ex1

--- material/lab_05/ex1/chenillard.c
+++ material/lab_05/ex1/chenillard.c
@@ -50,7 +50,7 @@ struct priv {
 };
 
 static int chaser_file_open(struct inode *inode, struct file *filp);
-static int chaser_file_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos);
+static ssize_t chaser_file_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos);
 
 static const struct file_operations chaser_fops = {
 	.owner = THIS_MODULE,
@@ -88,7 +88,7 @@ static void chaser_write(struct priv const *const priv, int const reg_offset, in
 	iowrite32(value, (int *)priv->mem_ptr + (reg_offset / 4));
 }
 
-static int chaser_file_open(struct inode *inode, struct file *filp) {
+static ssize_t chaser_file_open(struct inode *inode, struct file *filp) {
 	struct priv *priv;
 
 	// Register private data back in filp
@@ -105,14 +105,17 @@ static int chaser_file_open(struct inode *inode, struct file *filp) {
  */
 static int chaser_file_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos) {
 
-	struct priv *priv = filp->private_data;
+	struct priv *priv;
 	char tmp[5];
 	size_t len;
 
-	dev_info(priv->dev, "Received a job from write\n");
-
 	// check pointers
 	if (filp == NULL || buf == NULL) {
+		return -EINVAL;
+	}
+
+	priv = (struct priv *)filp->private_data;
+	if (priv == NULL) {
 		return -EINVAL;
 	}
 
@@ -121,9 +124,11 @@ static int chaser_file_write(struct file *filp, const char __user *buf, size_t c
 		return 0;
 	}
 
+	dev_info(priv->dev, "Received a job from write\n");
+
 	// check we can add a job
 	if (kfifo_is_full(&priv->jobs)) {
-		dev_info(priv->dev, "No space for more jobs");
+		dev_info(priv->dev, "No space for more jobs\n");
 		return -ENOMEM;
 	}
 
@@ -173,21 +178,21 @@ static int chaser_worker(void *data) {
 		if (kthread_should_stop()) break;
 
 		if (!kfifo_get(&priv->jobs, &job)) {
-			dev_info(priv->dev, "Tried to get a job from empty fifo");
+			dev_info(priv->dev, "Tried to get a job from empty fifo\n");
 			continue;
 		}
 
 		if (job == (uint8_t) UP) {
-			dev_info(priv->dev, "Processed up signal");
+			dev_info(priv->dev, "Processing up signal...\n");
 		} else if (job == (uint8_t) DOWN) {
-			dev_info(priv->dev, "Processed down signal");
+			dev_info(priv->dev, "Processing down signal...\n");
 		} else {
-			dev_info(priv->dev, "Tried to process an unknown signal");
+			dev_info(priv->dev, "Tried to process an unknown signal\n");
 			continue;
 		}
 
 		for (uint8_t i = 0; i < NB_LEDS && !kthread_should_stop(); ++i) {
-			chaser_write(priv, LED_REG_OFF, job == UP ? 1 << i : 1 << (NB_LEDS - i));
+			chaser_write(priv, LED_REG_OFF, job == UP ? 1 << i : 1 << (NB_LEDS - 1 - i));
 			msleep_interruptible(1000);
 		}
 		chaser_write(priv, LED_REG_OFF, 0);
@@ -294,7 +299,7 @@ static int chaser_probe(struct platform_device *pdev) {
 
 	// create a class in /sys/class
 	priv->dev_class = class_create(THIS_MODULE, DEV_NAME);
-	if (!priv->dev_class) {
+	if (IS_ERR(priv->dev_class)) {
 		dev_err(&pdev->dev, "Failed to allocate device's class\n");
 		goto free_chrdev;
 	}
@@ -334,10 +339,10 @@ free_cdev:
 	class_destroy(priv->dev_class);
 free_chrdev:
 	unregister_chrdev_region(priv->dev_num, 1);
-destroy_sysfs_group:
-	sysfs_remove_group(&pdev->dev.kobj, &chaser_attribute_group);
 free_thread:
 	kthread_stop(priv->thread);
+destroy_sysfs_group:
+	sysfs_remove_group(&pdev->dev.kobj, &chaser_attribute_group);
 free_kfifo:
 	kfifo_free(&priv->jobs);
 return_fail:
@@ -358,8 +363,7 @@ static int chaser_remove(struct platform_device *pdev) {
 
 	dev_info(&pdev->dev, "Turning system off...\n");
 
-	// destroy device in /dev
-	device_destroy(priv->dev_class, priv->dev_num);
+	// delete cdev
 	cdev_del(&priv->cdev);
 
 	// destroy class
@@ -368,17 +372,20 @@ static int chaser_remove(struct platform_device *pdev) {
 	// unregister character device
 	unregister_chrdev_region(priv->dev_num, 1);
 
-	// remove sysfs group
-	sysfs_remove_group(&pdev->dev.kobj, &chaser_attribute_group);
-
 	// free thread
 	if (priv->thread) {
 		kthread_stop(priv->thread);
 		wake_up_interruptible(&priv->worker_wait);
 	}
 
+	// remove sysfs group
+	sysfs_remove_group(&pdev->dev.kobj, &chaser_attribute_group);
+
 	// free kfifo
 	kfifo_free(&priv->jobs);
+
+	// destry device
+	device_destroy(priv->dev_class, priv->dev_num);
 
 	// clear everything
 	chaser_write(priv, LED_REG_OFF, 0x0);
